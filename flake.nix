ae40700d82e19299420c93c878ddde80f11c1140
jaykchen@icloud.com <jaykchen@icloud.com>
2024-12-10 12:12:09 -0500

    nix-ld needed

--- flake.nix
+++ flake.nix
@@ -1,80 +1,99 @@
-# flake.nix
 {
   description = "Python development environment with FHS, uv, and direnv";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-ld.url = "github:Mic92/nix-ld";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nix-ld }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = import nixpkgs {
             inherit system;
-            config.allowUnfree = true;
+            config = {
+              allowUnfree = true;
+            };
           };
-
+          # Create an FHS environment specifically for Python
           python-fhs-env = pkgs.buildFHSEnv {
             name = "python-fhs-env";
-            profileSetup = ''
-              export NIX_LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib
-              export NIX_LD_LIBRARY_PATH+=${pkgs.zlib}/lib
-              export NIX_LD_LIBRARY_PATH+=${pkgs.openssl}/lib
-              export NIX_LD_LIBRARY_PATH+=${pkgs.bzip2}/lib
-              export NIX_LD_LIBRARY_PATH+=${pkgs.libffi}/lib
-              export NIX_LD_LIBRARY_PATH+=${pkgs.ncurses}/lib
-              export NIX_LD_LIBRARY_PATH+=${pkgs.readline}/lib
-              export NIX_LD_LIBRARY_PATH+=${pkgs.xz}/lib
+
+            # Install Python and uv into the FHS environment
+            targetPkgs = pkgs: [ pkgs.uv ];
+
+            profileHook = ''
+              export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib:${pkgs.openssl.out}/lib:${pkgs.bzip2}/lib:${pkgs.libffi}/lib:${pkgs.ncurses}/lib:${pkgs.readline}/lib:${pkgs.xz}/lib
+
+              export PATH=${pkgs.stdenv.cc.cc.lib}/lib:$PATH
             '';
+
             runScript = "bash";
           };
+
         in
         {
           packages.python-fhs = python-fhs-env;
-          packages.uv = pkgs.uv;
           packages.direnv = pkgs.direnv;
 
-          devShells.default = pkgs.mkShell {
-            # Use the FHS environment as the base
-            inputsFrom = [ python-fhs-env ];
+          devShells.default =
+            let
+              libPath = pkgs.lib.makeLibraryPath [
+                pkgs.stdenv.cc.cc
+                pkgs.zlib
+                pkgs.openssl
+                pkgs.bzip2
+                pkgs.libffi
+                pkgs.ncurses
+                pkgs.readline
+                pkgs.xz
+              ];
+              fhsPython = python-fhs-env;
+            in
+            pkgs.mkShell {
+              # Set up nix-ld
+              LD_LIBRARY_PATH = libPath;
+              # Inherit the FHS environment
+              inputsFrom = [ fhsPython ];
+              # Bring in nix-ld
+              nativeBuildInputs = [
+                nix-ld.packages.${system}.nix-ld
+                pkgs.direnv
+              ];
 
-            # No need to add to `buildInputs` if the package is already in `targetPkgs`.
-            buildInputs = [
-              pkgs.direnv
-            ];
+              shellHook = ''
+                eval "$(direnv hook $0)"
 
-            shellHook = ''
-              eval "$(direnv hook $0)"
+                # Set up the virtual environment inside the FHS environment
+                if [[ ! -d .venv ]]; then
+                  echo "Creating virtual environment..."
+                  # Use uv from the FHS environment to create the virtual environment
+                  uv venv .venv
 
-              # Create and set up the virtual environment if it doesn't exist.
-              if [[ ! -d .venv ]]; then
-                echo "Creating virtual environment..."
-                # Use uv inside the FHS environment to create the venv
-                uv venv .venv
+                  echo ".venv" >> .gitignore
+                  echo "Virtual environment created in .venv"
+                fi
 
-                # Set up .gitignore
-                echo ".venv" >> .gitignore
-
-                echo "Virtual environment created in .venv"
-              fi
-
-              # Load the virtual environment
-              source .venv/bin/activate
-            '';
-          };
-        }) // {
-      # Templates
+                # Source to activate, but then...
+                source .venv/bin/activate
+                # Unset VIRTUAL_ENV and remove .venv/bin from PATH
+                unset VIRTUAL_ENV
+                export PATH=$(echo "$PATH" | sed -E "s|${builtins.replaceStrings [":"] [" "] ./.venv/bin}||")
+                # Make sure the FHS environment's python takes priority on PATH
+                export PATH=${fhsPython}/bin:$PATH
+              '';
+            };
+        }
+      ) // {
       templates = {
         python-fhs-uv-direnv = {
           path = ./.;
           description = "Python development environment with FHS, uv, and direnv";
-          welcomeText = ''
-            # Python Development Environment (FHS, uv, direnv)
-          '';
         };
         default = self.templates.python-fhs-uv-direnv;
       };
-    }; # Add the missing closing brace
+    };
 }
+
